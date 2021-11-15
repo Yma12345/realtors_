44551ef987051737da63cf9ed260102a218bb0b6
Yannick <82997@roc-teraa.nl>
2021-11-15 15:08:56 +0100

    includess

---
+++ inc/all-objects.php
@@ -0,0 +1,3 @@
+<div id="all-objects">
+
+</div>
--- index.php
+++ index.php
@@ -3,10 +3,8 @@ include("inc/header.php");
 include("inc/navbar.php");
 ?>
 <body>
-<div id="all-objects">
-
-</div>
 <?php
+include("inc/all-objects.php");
 include("inc/templates/all-objects.php");
 include("inc/templates/detail-objects.php");
 include("inc/modals/more-info.php");
