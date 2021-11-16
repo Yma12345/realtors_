df572293d9b67769b57abeacb2b6364d022b4caa
Yannick <82997@roc-teraa.nl>
2021-11-16 14:40:00 +0100

    project finished

--- admin.php
+++ admin.php
@@ -2,15 +2,12 @@
 
 <body>
   <?php include('inc/navbar.php'); ?>
+  
   <?php include('inc/templates/edit.php'); ?>
-
   <?php include('inc/admin-table.php'); ?>
   <?php include('inc/templates/admin-table-template.php'); ?>
-
   <?php include('inc/modals/edit-house.php'); ?>
-
   <?php include('inc/modals/create-house.php'); ?>
   <?php include('inc/admin-scripts.php'); ?>
 </body>
-
 </html>
